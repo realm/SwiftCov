36091a57995d533dbe49fcfee7a12f1b24819215
kishikawa katsumi <kishikawakatsumi@mac.com>
2015-06-01 01:53:05 +0900

    Add `debug` (verbose log) option

--- Source/SwiftCovFramework/Shell.swift
+++ Source/SwiftCovFramework/Shell.swift
@@ -13,18 +13,20 @@ import Result
 public typealias TerminationStatus = Int32
 
 public class Shell {
-    var commandPath: String
-    var arguments: [String]
-    var workingDirectoryPath: String?
-    var environment: [String: String]?
+    private let commandPath: String
+    private let arguments: [String]
+    private let workingDirectoryPath: String?
+    private let environment: [String: String]?
+    private let verbose: Bool
 
-    var outputString = ""
+    private var outputString = ""
 
-    public init(commandPath: String, arguments: [String] = [], workingDirectoryPath: String? = nil, environment: [String: String]? = nil) {
+    public init(commandPath: String, arguments: [String] = [], workingDirectoryPath: String? = nil, environment: [String: String]? = nil, verbose: Bool = false) {
         self.commandPath = commandPath
         self.arguments = arguments
         self.workingDirectoryPath = workingDirectoryPath
         self.environment = environment
+        self.verbose = verbose
     }
 
     public func run() -> Result<String, TerminationStatus> {
@@ -63,7 +65,9 @@ public class Shell {
         if let fileHandle = task.standardOutput.fileHandleForReading {
             fileHandle.readabilityHandler = { fileHandle in
                 if let string = NSString(data: fileHandle.availableData, encoding: NSUTF8StringEncoding) {
-                    fputs("\(string)", stdout)
+                    if self.verbose {
+                        fputs("\(string)", stdout)
+                    }
                     self.outputString += string as String
                 }
             }
@@ -71,7 +75,9 @@ public class Shell {
         if let fileHandle = task.standardError.fileHandleForReading {
             fileHandle.readabilityHandler = { fileHandle in
                 if let string = NSString(data: fileHandle.availableData, encoding: NSUTF8StringEncoding) {
-                    fputs("\(string)", stderr)
+                    if self.verbose {
+                        fputs("\(string)", stderr)
+                    }
                     self.outputString += string as String
                 }
             }
